af6c0bbd0c8a3caf9a162b9a5b8729607c10576a
Philip Schlump <pschlump@gmail.com>
2020-02-24 09:44:54 -0700

    Fixed exmple

--- Lectures/Lect-12/ex8.c
+++ Lectures/Lect-12/ex8.c
@@ -4,5 +4,5 @@ int main() {
 	int x;
 	x = 0x23;					/* Hex value */
 	x = x ^ 0xF0;				/* Set a single bit */
-	printf ( "0x%02x (Expect 0xE123)\n", x );	/* Print in Hex */
+	printf ( "0x%02x (Expect 0xD3)\n", x );	/* Print in Hex */
 }
